64f3f96a2fe8023485062f57a5f93ffd88e61d83
shymaSane <shymaSane1@gmail.com>
2017-12-05 12:40:38 +0200

    fix information to be able to access

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const request = require('request')
 
-request('http://maps.googleapis.com/maps/api/geocode/json?address=%20amman',
+request({ url: 'http://maps.googleapis.com/maps/api/geocode/json?address=%20amman',
+// we can access the location becouse the information we got back is
+    // stringified so what to do ?
+    // beside the url add josn:true 
+ json: true
+},
 (error, response, body) => {
-    console.log(body)
+    // to show the objects we got back 
+    //but it ll be messy so we have to decide how many lines to show so we ll add 2
+    console.log(JSON.stringify(body, undefined, 2))
 }
 )
