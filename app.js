b50f4d6c8f2d2782f90b237bca22e0c715a8f7f5
shymaSane <shymaSane1@gmail.com>
2017-12-05 13:50:24 +0200

    http request working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 request({ url: 'http://maps.googleapis.com/maps/api/geocode/json?address=%20amman',
-// we can access the location becouse the information we got back is
+// we can access the location because the information we got back is
     // stringified so what to do ?
     // beside the url add josn:true 
  json: true
@@ -9,6 +9,9 @@ request({ url: 'http://maps.googleapis.com/maps/api/geocode/json?address=%20amma
 (error, response, body) => {
     // to show the objects we got back 
     //but it ll be messy so we have to decide how many lines to show so we ll add 2
-    console.log(JSON.stringify(body, undefined, 2))
+    // console.log(JSON.stringify(body, undefined, 2))
+    console.log(`Address is: ${body.results[0].formatted_address}`)
+    console.log(`location lat: ${body.results[0].geometry.location.lat}`)
+    console.log(`location lng: ${body.results[0].geometry.location.lng}`)
 }
 )
