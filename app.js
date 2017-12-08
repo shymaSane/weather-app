105d550a6fdc29b5b7867854e7acd244af2c3020
shymaSane <shymaSane1@gmail.com>
2017-12-08 20:30:41 +0200

    handling callbacks errors

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 
-const request = require('request')
-const yargs = require('yargs')
+const request = require('request');
+const yargs = require('yargs');
 const argv = yargs
 .options({
-    //since we dont have any commands for this project we are using optons to determine 
+    //since we dont have any commands for this project we are using options to determine 
     //what rules we want to apply.
     // to test it write node app.js -a 'amman'
     'a': {
         alias: 'address',
         demandOption: true,
         describe: 'you need the address to know the weather',
-        type: 'string'
+        string: true
       }
 })
 .help()
-.argv
+.argv; 
 
-request({ url: 'http://maps.googleapis.com/maps/api/geocode/json?address=%20amman',
+// to get the address
+var address = argv.address;
+//to encode the address in order to add it to the url
+var encodeAddress = encodeURIComponent(address);
+
+
+request({ url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
 // we can access the location because the information we got back is
     // stringified so what to do ?
     // beside the url add josn:true 
  json: true
 },
 (error, response, body) => {
+    // if we didnt put the if statement then when we entered a wrong adress the program ll crash 
+    // now the first condition when we are not able to connect the api
+    if(error) {
+        console.log('Unable to connect Google api.');
+    }
+    //when the user entered a wrong address
+    // the status is different in every api so you have to try to know the condition 
+    else if (body.status === "ZERO_RESULTS"){
+        console.log("You entered the wrong address pleas try again.");
+    }
+    //when the user dont enter an addressa(this in case we dont demand in the argv options)
+    else if (body.status === "INVALID_REQUEST"){
+        console.log("Please enter your adress");
+    }
+    else if (body.status === "OK") {
     // to show the objects we got back 
     //but it ll be messy so we have to decide how many lines to show so we ll add 2
     // console.log(JSON.stringify(body, undefined, 2))
-    console.log(`Address is: ${body.results[0].formatted_address}`)
-    console.log(`location lat: ${body.results[0].geometry.location.lat}`)
-    console.log(`location lng: ${body.results[0].geometry.location.lng}`)
+    console.log(`Address is: ${body.results[0].formatted_address}`);
+    console.log(`location lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`location lng: ${body.results[0].geometry.location.lng}`);
+
+    }
 }
 )
